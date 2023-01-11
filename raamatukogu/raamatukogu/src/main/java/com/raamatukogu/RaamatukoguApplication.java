f34d2bed3c72e0d153aea3d75cf3fbaa6839e1f4
Aron Miljan <122377077+aemiljan@users.noreply.github.com>
2023-01-11 02:27:08 +0200

    Add files via upload
    Initial Spring files

---
+++ raamatukogu/raamatukogu/src/main/java/com/raamatukogu/RaamatukoguApplication.java
@@ -0,0 +1,13 @@
+package com.raamatukogu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaamatukoguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaamatukoguApplication.class, args);
+	}
+
+}
---
+++ raamatukogu/raamatukogu/src/test/java/com/raamatukogu/RaamatukoguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raamatukogu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaamatukoguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
